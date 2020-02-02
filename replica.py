4934e467c9dac4158bcf28dd1280839a6599fea4
Gregory Wint <gregwint95@gmail.com>
2020-02-01 20:52:50 -0500

    Added a .env file to hold system configuration information and created function to calculate an election timeout value

--- replica.py
+++ replica.py
@@ -1,8 +1,18 @@
+import logging
+from dotenv import load_dotenv
+from os import getenv
+from random import randint
+
 from states import ReplicaState
 from lockhandler import LockHandler
 
 class Replica:
+    MIN_ELECTION_TIMEOUT_ENV_VAR_NAME = "RANDOM_TIMEOUT_MIN_MS"
+    MAX_ELECTION_TIMEOUT_ENV_VAR_NAME = "RANDOM_TIMEOUT_MAX_MS"
+
     def __init__(self):
+        load_dotenv()
+
         self._state = ReplicaState.FOLLOWER
         self._currentTerm = 0
         self._log = []
@@ -10,8 +20,20 @@ class Replica:
         self._lastApplied = 0
         self._nextIndex = []
         self._matchIndex = []
+        self._timeout = getElectionTimeout()
 
         self.lockHandler = LockHandler(7)
 
     def __str__(self):
         return ""
+
+    def _getElectionTimeout(self):
+        minTimeMS = getenv(Replica.MIN_ELECTION_TIMEOUT_ENV_VAR_NAME)
+        maxTimeMS = getenv(Replica.MAX_ELECTION_TIMEOUT_ENV_VAR_NAME)
+
+        if not minTimeMS:
+            raise ValueError("Attempted to read value from nonexistent enviornemnt variable {Replica.MIN_ELECTION_TIMEOUT_ENV_VAR_NAME}")
+        if not maxTimeMS:
+            raise ValueError("Attempted to read value from nonexistent enviornemnt variable {Replica.MAX_ELECTION_TIMEOUT_ENV_VAR_NAME}")
+
+        return randint(int(minTimeMS), int(maxTimeMS))
