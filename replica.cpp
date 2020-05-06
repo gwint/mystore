f196bba9b2b6dd25471de1c5b2ec8148275ea27b
Gregory Wint <gregwint95@gmail.com>
2020-05-06 16:43:17 -0400

    Add areAMajorityGreaterThanOrEqual function to replica.cpp

--- replica.cpp
+++ replica.cpp
@@ -10,7 +10,20 @@
 #include <thrift/protocol/TBinaryProtocol.h>
 #include <thrift/server/TThreadedServer.h>
 
-//#include "gen-cpp/replicaservice_types.h"
+#include "gen-cpp/replicaservice_types.h"
+
+bool
+areAMajorityGreaterThanOrEqual(std::vector<unsigned int> numLst, unsigned int num) {
+    unsigned int numForMajority = (numLst.size() / 2) + 1;
+    unsigned int numGreaterThanOrEqual = 0;
+    for(const unsigned int& currNum : numLst) {
+        if(currNum >= num) {
+            ++numGreaterThanOrEqual;
+        }
+    }
+
+    return numGreaterThanOrEqual >= numForMajority;
+}
 
 int main() {
     std::cout << "Now running..." << '\n';
