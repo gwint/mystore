3042fe382a751e402d5a60a581cda4040f63d058
Gregory Wint <gregwint95@gmail.com>
2020-05-06 18:04:08 -0400

    Add spdlog dependancy to CMakeLists.txt

--- replica.cpp
+++ replica.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <string>
 
 #include "replica.hpp"
 #include "lockhandler.hpp"
 #include "locknames.hpp"
 #include "states.hpp"
 
+#include "spdlog/spdlog.h"
 #include <thrift/transport/TSocket.h>
 #include <thrift/transport/TTransport.h>
 #include <thrift/protocol/TBinaryProtocol.h>
@@ -24,6 +26,16 @@ areAMajorityGreaterThanOrEqual(std::vector<unsigned int> numLst, unsigned int nu
 
     return numGreaterThanOrEqual >= numForMajority;
 }
+
+struct Job {
+    Job(int entryPositionIn,
+        std::string targetHostIn,
+        unsigned int targetPortIn) : entryPosition(entryPositionIn), targetHost(targetHostIn), targetPort(targetPortIn) {}
+
+    int entryPosition;
+    std::string targetHost;
+    unsigned int targetPort;
+};
 
 int main() {
     std::cout << "Now running..." << '\n';
